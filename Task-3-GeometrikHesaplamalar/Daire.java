742036d4980dbc3ab8573c1556db689bd12b3318
Kerem <keremkucuk023@gmail.com>
2025-10-22 19:34:58 +0300

    Create Daire.java

---
+++ Task-3-GeometrikHesaplamalar/Daire.java
@@ -0,0 +1,17 @@
+public class Daire {
+    public static void main(String[] args) {
+        double r=5.5;
+        double pi=3.14;
+
+        // Alan Hesapla
+        double alan= pi * r * r;
+
+        // Çevre Hesapla
+        double cevre=2*pi*r;
+
+        System.out.println("Alan: "+alan);
+        System.out.println("Çevre: "+cevre);
+        System.out.println("yarıçap: "+r);
+
+    }
+}
