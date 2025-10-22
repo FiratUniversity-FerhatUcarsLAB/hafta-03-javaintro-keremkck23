18abebaf6f4573dd4803e0bb18abc9dfadc6f120
Kerem <keremkucuk023@gmail.com>
2025-10-22 18:26:04 +0300

    Add JavaDeseni class to print Java pattern

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,9 @@
+public class JavaDeseni {
+    public static void main(String[] args) {
+        System.out.println("  J A V A ");
+        System.out.println(" J A V A A");
+        System.out.println("J AA   V  A ");
+        System.out.println(" J AAA V V   A");
+        System.out.println("  J AAA   VVV  AAA ");
+    }
+}
