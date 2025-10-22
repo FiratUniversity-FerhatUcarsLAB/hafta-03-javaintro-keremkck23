81597c14d5108774882e3059e97f3e9c43a61110
Kerem <keremkucuk023@gmail.com>
2025-10-23 00:25:57 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,23 @@
+public class IsimKartı {
+    public static void main(String[] args) {
+        String ad="Kerem";
+        String soyad="Küçük";
+        String Ogrno="250541097";
+        String fakulte="Teknoloji";
+        String Bolum="Yazılım";
+
+
+
+        System.out.println("------------");
+       System.out.println("Öğrenci Bilgileri");
+
+
+        System.out.println("ad: "+ad);
+        System.out.println("soyad: "+soyad);
+        System.out.println("No: "+Ogrno);
+        System.out.println("Fakulte: "+fakulte);
+        System.out.println("Bolum: "+Bolum);
+
+
+    }
+}
