227df335ce3048dce4aebeb8b7de6a12d97ca4c0
Kerem <keremkucuk023@gmail.com>
2025-10-22 18:22:07 +0300

    Add UcMesaj class with welcome messages

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,5 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Javaya hoş geldiniz");
+        System.out.println("Bilgisayar bilimine hoşgeldiniz");
+        System.out.println("programlama eğlencelidir");
