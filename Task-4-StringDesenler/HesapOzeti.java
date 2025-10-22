b6cd99a7d711be69fb545f0a0d47b1962154f7dd
Kerem <keremkucuk023@gmail.com>
2025-10-23 00:51:46 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,17 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+        String urun="Cips";
+        int miktar=14;
+        double br_fiyat=12.5;
+        double Toplam_fiyat=miktar*br_fiyat;
+
+        System.out.println("================");
+        System.out.println("|"+" "+"ürün: "+urun);
+        System.out.println("|"+" "+"birim fiyat: "+br_fiyat);
+        System.out.println("|"+" "+"Toplam fiyat: "+Toplam_fiyat);
+        System.out.println("|"+"=============");
+
+
+    }
+}
+
