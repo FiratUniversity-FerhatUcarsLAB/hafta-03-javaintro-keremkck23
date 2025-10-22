60dd292748042d35ae0e31ca61a2f7b9fa4be79e
Kerem <keremkucuk023@gmail.com>
2025-10-23 01:24:53 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,12 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+        int mil=1609;
+        System.out.println("Mil Kilometre Tablosu");
+        System.out.println("===================");
+        System.out.println("mil: "+mil);
+        System.out.println("5mil: "+mil*5);
+        System.out.println("10mil: "+mil*10);
+        System.out.println("20mil: "+mil*20);
+        System.out.println("50mil: "+mil*50);
+    }
+}
