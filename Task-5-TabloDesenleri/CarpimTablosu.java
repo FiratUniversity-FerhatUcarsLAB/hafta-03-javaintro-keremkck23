41647169ab6cb01f72f8cde855fc4b74631a6aff
Kerem <keremkucuk023@gmail.com>
2025-10-23 01:15:50 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,17 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        System.out.println("2 nin carpım tablosu");
+        System.out.println("-------------------");
+        System.out.println("2x1=2");
+        System.out.println("2x2=4");
+        System.out.println("2x3=6");
+        System.out.println("2x4=8");
+        System.out.println("2x5=10");
+        System.out.println("2x6=12");
+        System.out.println("2x7=14");
+        System.out.println("2x8=16");
+        System.out.println("2x9=18");
+        System.out.println("2x10=20");
+
+    }
+}
