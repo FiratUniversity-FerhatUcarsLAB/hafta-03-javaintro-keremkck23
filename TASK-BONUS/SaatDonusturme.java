a369c542159840571e25438e5c2094bfc717acdc
Kerem <keremkucuk023@gmail.com>
2025-10-23 15:17:25 +0300

    Update SaatDonusturme.java

---
+++ TASK-BONUS/SaatDonusturme.java
@@ -0,0 +1,10 @@
+public class SaatDonusturme {
+    public static void main(String[] args) {
+        int saniye= 7200;
+        int dakika= saniye/60;
+        int saat=dakika/60;
+        System.out.println("saat: "+saat+":00:00");
+
+
+    }
+}
