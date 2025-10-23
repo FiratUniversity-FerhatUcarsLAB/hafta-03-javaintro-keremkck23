92ac1f98f75a1fc7cac076d629b752ef665e6c10
Kerem <keremkucuk023@gmail.com>
2025-10-23 12:13:24 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,11 @@
+public class VucutKitlelndeksi {
+    public static void main(String[] args) {
+        double boy=1.80;
+        int kilo=75;
+        double BIM=kilo/(boy*boy);
+        System.out.println("kilo: "+kilo);
+        System.out.println("boy "+boy);
+        System.out.println("vucut kitle endeksi=kilo/(boy*boy)");
+        System.out.println("Vucut kitle endeksi: "+BIM);
+    }
+}
